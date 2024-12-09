f4fe1eea0f9a375bbafd7e47b41e07660caeeb12
Maycon Ruan Mota Pereira <mayconpereira402017@gmail.com>
2024-12-08 22:08:13 -0300

    enviando-2

--- js/menu.js
+++ js/menu.js
@@ -1,40 +1,4 @@
-//Deixar item-menu selecionado
-// var menuitem = document.querySelectorAll('.item-menu')
-
-// function selectLink(){
-//     menuitem.forEach((item)=>
-//         item.classList.remove('ativo')
-//     )
-//     this.classList.add('ativo')
-// }
-
-// menuitem.forEach((item)=>
-//     item.addEventListener('click', selectLink)
-// )
-
-// Expandir o menu
-
-// //Principal
-// var btnExp = document.querySelector('#icon-seta')
-// var menuSide = document.querySelector('.menu-lateral')
-
-// btnExp.addEventListener('click', function(){
-//     menuSide.classList.toggle('expandir')
-// })
-// //Container 1
-// var btnExp2 = document.querySelector('#icon-seta')
-// var menuSide2 = document.querySelector('.container-itens-menu')
-
-// btnExp2.addEventListener('click', function(){
-//     menuSide2.classList.toggle('expandir')
-// });
-// //Container 2
-// var btnExp3 = document.querySelector('#icon-seta')
-// var menuSide3 = document.querySelector('.container-itens-menu2')
-
-// btnExp3.addEventListener('click', function(){
-//     menuSide3.classList.toggle('expandir')
-// });
+// Abrir subMenu
 
 document.addEventListener("DOMContentLoaded", () => {
     const menuItems = document.querySelectorAll(".item-menu .txt-link");
@@ -68,5 +32,75 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+// Filtro de preço - barra
+
+const rangeInput = document.querySelectorAll(".range-input input"),
+    priceInput = document.querySelectorAll(".input-preco input"),
+    progress = document.querySelector(".slider .progress");
+
+let priceGap = 1000;
+
+// Atualizar rangeInput ao modificar priceInput
+priceInput.forEach((input, index) => {
+    input.addEventListener("input", () => {
+        let minPrice = parseInt(priceInput[0].value),
+            maxPrice = parseInt(priceInput[1].value);
+
+        // Garantir o gap mínimo entre os valores
+        if (maxPrice - minPrice >= priceGap && maxPrice <= 10000 && minPrice >= 0) {
+            if (index === 0) {
+                rangeInput[0].value = minPrice;
+                progress.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+            } else {
+                rangeInput[1].value = maxPrice;
+                progress.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    });
+});
+
+// Atualizar priceInput ao modificar rangeInput
+rangeInput.forEach(input => {
+    input.addEventListener("input", e => {
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
+
+        // Garantir o gap mínimo entre os controles
+        if (maxVal - minVal < priceGap) {
+            if (e.target.className === "range-min") {
+                rangeInput[0].value = maxVal - priceGap;
+            } else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+        } else {
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
+
+
+// Vereficar input
+
+const searchInput = document.querySelector('.search-input');
+const brandList = document.querySelectorAll('.lista-marcas label');
+
+searchInput.addEventListener('input', () => {
+    const filter = searchInput.value.toLowerCase();
+
+    brandList.forEach(label => {
+        const text = label.textContent.toLowerCase();
+        if (text.includes(filter)) {
+            label.style.display = 'flex'; // Mostrar a marca correspondente
+        } else {
+            label.style.display = 'none'; // Ocultar a marca não correspondente
+        }
+    });
+});
+
 
 
